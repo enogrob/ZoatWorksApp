1e9d1ec3ad52c6af184080b23e196376da850a39
enogrob <enogrob@gmail.com>
2013-11-18 14:55:31 -0200

    third commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def title
+    base_title = "Crafted (C) by ZoatWorks Software LLC"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,13 +1,4 @@
 module PagesHelper
 
-def title
-  # Return a title on a per-page basis.
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if @title.nil?
-    base_title
-  else
-    "#{base_title} | #{@title}"
-  end
-end
 
 end
