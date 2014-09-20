678aadc01c7fa2e8c5d72fc9a4a65c1dcc144e53
Roberto Nogueira <enogrob@gmail.com>
2014-09-20 08:37:54 -0300

    User model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,21 @@ class User < ActiveRecord::Base
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   attr_accessor :password
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password_confirmation
 
   validates :name, :presence => true, :length => {:maximum => 50}
   validates :email, :presence => true, :format => {:with => email_regex}
   validates :password, :presence => true, :confirmation => true, :length => {:within => 6..40}
+
+  before_save :encrypted_password
+
+  private
+
+  def encrypted_password
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(string)
+    string
+  end
 end
